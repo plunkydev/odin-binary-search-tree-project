e6c8f50a6932ad1d8d77d99952dca55ab3b610a6
plunkydev <plunkydesign@gmail.com>
2025-09-18 15:11:35 -0500

    feat: add prettyPrint helper to utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,13 @@
 // prettyPrint y helpers
+export const prettyPrint = (node, prefix = '', isLeft = true) => {
+    if (node === null) {
+        return
+    }
+    if (node.right !== null) {
+        prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false)
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`)
+    if (node.left !== null) {
+        prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true)
+    }
+}
