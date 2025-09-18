e9c23d93b73054766f380e21af0b5fc40ce889a3
plunkydev <plunkydesign@gmail.com>
2025-09-18 15:13:55 -0500

    feat: add Tree class with buildTree method to tree.js

--- src/tree.js
+++ src/tree.js
@@ -1,1 +1,18 @@
 // clase Tree + buildTree + API pública
+import { Node } from './node'
+
+export class Tree {
+  constructor (array) {
+    array = array.sort((a, b) => a - b)
+    this.root = this.buildTree(array)
+  }
+
+  buildTree (array) {
+    if (array.length === 0) return null
+    const middle = Math.floor(array.length / 2)
+    const node = new Node(array[middle])
+    node.left = this.buildTree(array.slice(0, middle))
+    node.right = this.buildTree(array.slice(middle + 1))
+    return node
+  }
+}
